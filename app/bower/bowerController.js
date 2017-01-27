c9b98914c477e7f5804af83090527d37f4789dca
VIJAYA BHASKARA Reddy <Chinnu@VIJAYAs-MacBook-Air.local>
2017-01-27 19:24:21 +0000

    added content for january 64th

--- app/bower/bowerController.js
+++ app/bower/bowerController.js
@@ -3,14 +3,16 @@
 myApp.controller('bowerController',['$log','$scope',function($log,$scope){
 
 	$scope.bowerQuestionsList = [{id:'0',name:'introduction',display:'Introduction to Bower'},
-								 {id:'1',name:'install',display:'How to install and use Bower'}];
-	$scope.selected = 0;
+								 {id:'1',name:'install',display:'How to install and use Bower'},
+								 {id:'2',name:'configure',display:'Configure Bower for new project'}];
+	$scope.selected = {value:0};
+	$scope.selected.value = 0;
 
 	$scope.changeView = function(viewName){
 		for(var i in $scope.bowerQuestionsList){
 			var question = $scope.bowerQuestionsList[i];
 			if(question.name === viewName){
-				$scope.selected = question.id;
+				$scope.selected.value = question.id;
 			}
 		}
 	}
